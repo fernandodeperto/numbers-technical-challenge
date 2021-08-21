95a71c189109a3b8f8552eb3ffc446709f0b8014
Fernando Mendonca <f.mendonca@quin.md>
2021-08-21 23:21:14 +0200

    print version on the magnusapi response

--- magnusapi/magnusapi/api.py
+++ magnusapi/magnusapi/api.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 
+from magnusapi.version import __version__
+
 app = FastAPI()
 
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {
+        "message": "Hello World",
+        "version": __version__,
+    }
