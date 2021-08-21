590256d550b02facc56d2b2b2d2d6a450f85548c
Fernando Mendonca <f.mendonca@quin.md>
2021-08-21 23:57:14 +0200

    attempt at adding db support to fastapi

--- magnusapi/magnusapi/api.py
+++ magnusapi/magnusapi/api.py
@@ -1,6 +1,23 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
 
 from magnusapi.version import __version__
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -11,3 +28,14 @@ def root():
         "message": "Hello World",
         "version": __version__,
     }
+
+
+@app.post("/item/", response_model=schemas.Item)
+def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    return crud.create_user_item(db=db, item=item)
+
+
+@app.get("/items/", response_model=List[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
